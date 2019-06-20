9ee54cb081ba641d77b24b77f7fd73df4e078d45
Li Ao <liao6@teach.cs.toronto.edu>
2019-06-19 23:50:32 -0400

    Keep working on

--- lab/lab7/pwd.c
+++ lab/lab7/pwd.c
@@ -5,24 +5,51 @@
 int main(){
 struct stat statbuf1;
 struct dirent *p;
-char file_name[3] = {'.', '\0'};
+char file_name []= {'.', '\0'};
 char parent_dir[] = {'.', '.', '\0'};
 DIR *dp;
 long current_ino;
 if(lstat(file_name, &statbuf1)!=1){
-current_ino = statbuf1.st_ino;
-printf("%ld\n", current_ino);
+    current_ino = statbuf1.st_ino;
+    printf("The current inode is %ld\n", current_ino);
 }
-if(chdir(parent_dir)!=1){
+else{
+    // Write error
+    return 1;
+}
+// Change directory to parent directory
+// Loop over every node in the parent directory to find the corresponding directory
+while(chdir(parent_dir)!=1 && statbuf1.st_ino != 2){
+    // Change back to parent dir
+    dp = opendir(file_name);
+    if(dp != NULL){
+        // Open everything in the parent directory
+        while((p=readdir(dp))){
+            // If find the thing we want print its name
+            if((p->d_ino == current_ino)){
+                printf("%s\n", p->d_name);
+            }
+        }
+        closedir(dp);
+	// Get info of parent directory and change back to grandparent directory
+        lstat(file_name, &statbuf1);
+	current_ino = statbuf1.st_ino;
+	printf("%ld\n", current_ino);
+    }
+    else{
+    //Write error
+    return 1;
+    }
+}
+lstat(file_name, &statbuf1);
+printf("%ld\n", statbuf1.st_ino);
 dp = opendir(file_name);
 if(dp != NULL){
-while((p=readdir(dp))){
-if((p->d_ino == current_ino)){
-printf("%s\n", p->d_name);
+while((p = readdir(dp))){
+	printf("%ld\n", p->d_ino);
+        printf("%s\n", p->d_name);
 }
 }
 closedir(dp);
-}
-}
 return 0;
 }
