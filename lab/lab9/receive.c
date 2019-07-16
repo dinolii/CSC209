a307bd3c186066724d4c6059e1aa1e9a2fe3c8f3
dino <dino.li@mail.utoronto.ca>
2019-07-15 20:46:16 -0400

    Working on it

---
+++ lab/lab9/receive.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+int count = 0;
+int sig = 0
+void receive_signal(int signal){
+    sig <<= 1;
+    if(signal == SIGUSR2){
+        sig += 1;
+    }
+    count += 1;
+    if(count == 8){
+        printf("%c\n", sig);
+    }
+}
+
+int main(){
+    printf("%d\n", getpid());
+    signal(SIGUSR1, receive_signal);
+    signal(SIGUSR2, receive_signal);
+    while(1);
+    return 0;
+}
---
+++ lab/lab9/transmit.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+
+int main(int argc, char **argv){
+    int pid = atoi(argv[1]);
+    char temp = *(argv[2]);
+    int count = 0;
+    while(count < 8){
+        if(temp & 128){
+            kill(pid, SIGUSR2);
+        }
+        else{
+            kill(pid, SIGUSR1);
+        }
+        temp <<= 1;
+        coutn ++;
+        usleep(100000);
+    }
+}
